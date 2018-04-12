3031cfa69049f5cc3f29256b548402d65db0b2a0
Frederick Hornsey <fred@hornsey.us>
2018-04-11 23:23:27 -0500

    fb.c: Fix indent and add lock

--- src/platform/x86/x86_32/fb.c
+++ src/platform/x86/x86_32/fb.c
@@ -5,39 +5,38 @@
 
 #include "fb.h"
 #include "io.h"
- 
+
+lock_t fb_lock;
 u4 fb_row;
 u4 fb_column;
 fb_color_t fb_color;
 u2 * fb_buffer;
 
-lock_t fb_lock;
- 
 void fb_set_color(fb_color_t fg, fb_color_t bg) {
-	fb_color = fg | bg << 4;
+    fb_color = fg | bg << 4;
 }
- 
+
 static inline u2 fb_color_char(unsigned char uc, fb_color_t color) {
-	return (u2) uc | (u2) color << 8;
+    return (u2) uc | (u2) color << 8;
 }
- 
+
 void fb_initialize(void) {
-	fb_row = 0;
-	fb_column = 0;
     fb_lock = UNLOCKED;
-	fb_set_color(FB_COLOR_LIGHT_GREY, FB_COLOR_BLACK);
-	fb_buffer = (u2*) kernel_offset(0xB8000);
-	for (u4 y = 0; y < FB_HEIGHT; y++) {
-		for (u4 x = 0; x < FB_WIDTH; x++) {
-			const u4 index = y * FB_WIDTH + x;
-			fb_buffer[index] = fb_color_char(' ', fb_color);
-		}
-	}
+    fb_row = 0;
+    fb_column = 0;
+    fb_set_color(FB_COLOR_LIGHT_GREY, FB_COLOR_BLACK);
+    fb_buffer = (u2*) kernel_offset(0xB8000);
+    for (u4 y = 0; y < FB_HEIGHT; y++) {
+        for (u4 x = 0; x < FB_WIDTH; x++) {
+            const u4 index = y * FB_WIDTH + x;
+            fb_buffer[index] = fb_color_char(' ', fb_color);
+        }
+    }
     fb_cursor(0, 0);
 }
- 
+
 void fb_cursor(u4 x, u4 y) {
-	const u4 index = y * FB_WIDTH + x;
+    const u4 index = y * FB_WIDTH + x;
     out1(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
     out1(FB_DATA_PORT, ((index >> 8) & 0x00FF));
     out1(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
@@ -45,29 +44,27 @@ void fb_cursor(u4 x, u4 y) {
 }
 
 void fb_place_char(char c, fb_color_t color, u4 x, u4 y) {
-	const u4 index = y * FB_WIDTH + x;
-	fb_buffer[index] = fb_color_char(c, color);
+    const u4 index = y * FB_WIDTH + x;
+    fb_buffer[index] = fb_color_char(c, color);
 }
 
 void scroll() {
     for (u4 y = 1; y < FB_HEIGHT; y++) {
-		for (u4 x = 0; x < FB_WIDTH; x++) {
-			const u4 src = y * FB_WIDTH + x;
-			const u4 dest = (y-1) * FB_WIDTH + x;
-			fb_buffer[dest] = fb_buffer[src];
-		}
+        for (u4 x = 0; x < FB_WIDTH; x++) {
+            const u4 src = y * FB_WIDTH + x;
+            const u4 dest = (y-1) * FB_WIDTH + x;
+            fb_buffer[dest] = fb_buffer[src];
+        }
     }
     for (u4 x = 0; x < FB_WIDTH; x++) {
         fb_buffer[FB_WIDTH * (FB_HEIGHT-1) + x] = fb_color_char(' ', fb_color);
     }
 }
- 
+
 void fb_print_char(char c) {
-    /*
     while (acquire_lock(&fb_lock)) {
+        // TODO: Sleep?
     }
-    */
-    disable_interrupts();
     if (c == '\n') {
         fb_column = 0;
         if (fb_row == (FB_HEIGHT-1)) {
@@ -87,6 +84,6 @@ void fb_print_char(char c) {
         fb_place_char(c, fb_color, fb_column, fb_row);
         fb_cursor(fb_column + 1, fb_row);
     }
-    enable_interrupts();
+    release_lock(&fb_lock);
 }
 
