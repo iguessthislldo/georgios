651df25a951bf33c5d14b14b816197bb81812964
Fred Hornsey <fred@hornsey.us>
2019-06-01 00:37:03 -0500

    Correct Port for ATA Status

--- kernel/platform/ata.c
+++ kernel/platform/ata.c
@@ -21,12 +21,12 @@
 #define CMD_HEAD_LBA (1 << 6)
 #define CMD_HEAD_SELECT(DRIVE) ((DRIVE) << 4)
 #define CMD_STATUS 7
+#define CMD_STATUS_BUSY (1 << 7)
+#define CMD_STATUS_READY (1 << 6)
+#define CMD_STATUS_ERROR (1 << 0)
 #define CMD_COMMAND 7
 
 #define CTL_ALT_STATUS 2
-#define CTL_ALT_STATUS_BUSY (1 << 7)
-#define CTL_ALT_STATUS_READY (1 << 6)
-#define CTL_ALT_STATUS_ERROR (1 << 0)
 #define CTL_DEV 2
 #define CTL_DEV_RESET (1 << 2)
 #define CTL_DEV_INT_ENABLE (0 << 1)
@@ -72,14 +72,15 @@ static inline u1 ctl_reg_read(u1 channel, u2 reg) {
  * otherwise false.
  */
 static inline bool channel_wait(u1 channel, u4 timeout) {
-    u2 result;
-    while (
-        (result = (ctl_reg_read(channel, CTL_ALT_STATUS) & CTL_ALT_STATUS_BUSY))
-        && timeout--
-        ) {
-        msec_wait(1000);
+    u1 result;
+    while (timeout--) {
+        result = cmd_reg_read(channel, CMD_STATUS);
+        if (!(result & CMD_STATUS_BUSY) && (result & CMD_STATUS_READY)) {
+            return false;
+        }
+        msec_wait(10);
     }
-    return result ? true : false;
+    return true;
 }
 
 #define RETURN(V) return (channels[channel].initialized = (V));
@@ -87,32 +88,34 @@ static inline bool channel_wait(u1 channel, u4 timeout) {
  * Try to Initialize Channel. Return false for error otherwise true
  */
 static inline bool initialize_drive(u1 channel, u1 drive) {
+    print_format("ATA {s} {s}: ",
+        channel ? "SECONDARY" : "PRIMARY",
+        drive ? "SLAVE" : "MASTER");
     cmd_reg_write(channel, CMD_HEAD, CMD_HEAD_SELECT(drive));
     ctl_reg_write(channel, CTL_DEV, CTL_DEV_INT_DISABLE | CTL_DEV_RESET);
     usec_wait(5);
     ctl_reg_write(channel, CTL_DEV, CTL_DEV_INT_DISABLE);
     msec_wait(3);
     if (channel_wait(channel, 30)) {
-        print_char('T');
+        print_string("Missing\n");
         RETURN(false);
     }
     msec_wait(2);
     u1 error = cmd_reg_read(channel, CMD_ERROR);
     u1 status = cmd_reg_read(channel, CMD_STATUS);
     if (error & 0x80 || status & 0x2D) {
-        print_format("Error is {x}, status is {x}\n", error, status);
+        print_format("Error\n    Error is {x}, status is {x}\n", error, status);
         RETURN(false);
     }
     cmd_reg_write(channel, CMD_HEAD, CMD_HEAD_SELECT(drive));
     usec_wait(5);
     u2 count = cmd_reg_read(channel, CMD_SEC_COUNT) & 0xFF;
     u2 number = cmd_reg_read(channel, CMD_SEC_NUMBER) & 0xFF;
-    print_format("count: {d}\n", count);
-    print_format("number: {d}\n", number);
     if (count != 1 || number != 1) {
-        print_char('V');
+        print_format("Error\n    Expected 1 and 1, but got {d} and {d}\n", count, number);
         RETURN(false);
     }
+    print_string("Present\n");
     RETURN(true);
 }
 
@@ -133,16 +136,10 @@ void ata_initialize_controller(u1 bus, u1 device, u1 function) {
     channels[SECONDARY].command_base = PORT(bar2, LEGACY_SECONDARY_CMD_BASE);
     channels[SECONDARY].control_base = PORT(bar3, LEGACY_SECONDARY_CTL_BASE);
 
-    print_format("ATA PRIMARY MASTER {s}\n",
-        initialize_drive(PRIMARY, MASTER) ? "Present" : "Missing");
-#if 0
-    print_format("ATA PRIMARY SLAVE {s}\n",
-        initialize_drive(PRIMARY, SLAVE) ? "Present" : "Missing");
-    print_format("ATA SECONDARY MASTER {s}\n",
-        initialize_drive(SECONDARY, MASTER) ? "Present" : "Missing");
-    print_format("ATA SECONDARY SLAVE {s}\n",
-        initialize_drive(SECONDARY, SLAVE) ? "Present" : "Missing");
-#endif
+    initialize_drive(PRIMARY, MASTER);
+    initialize_drive(PRIMARY, SLAVE);
+    initialize_drive(SECONDARY, MASTER);
+    initialize_drive(SECONDARY, SLAVE);
 }
 
 bool ata_disk_read(u1 disk, u4 sector, mem_t dest) {
