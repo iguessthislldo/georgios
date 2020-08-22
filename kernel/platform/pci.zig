a66d7703c9797e7df7892857dd05b0d1627967b1
Fred Hornsey <fred@hornsey.us>
2020-08-22 17:17:37 -0500

    Commit PCI as is...

--- kernel/platform/pci.zig
+++ kernel/platform/pci.zig
@@ -5,23 +5,10 @@ const builtin = @import("builtin");
 
 const kutil = @import("../util.zig");
 const print = @import("../print.zig");
+const fprint = @import("../fprint.zig");
+const io = @import("../io.zig");
 
 const putil = @import("util.zig");
-
-// Fields Common to all PCI Structures
-// Offset | Size | Name
-// 0x00   | 2    | Vendor ID
-// 0x02   | 2    | Device ID
-// 0x04   | 2    | Command
-// 0x06   | 2    | Status
-// 0x08   | 1    | Revision ID
-// 0x09   | 1    | Prog IF
-// 0x0A   | 1    | Subclass
-// 0x0B   | 1    | Class
-// 0x0C   | 1    | Cache Line Size
-// 0x0D   | 1    | Latency Timer
-// 0x0E   | 1    | Header Type
-// 0x0F   | 1    | BIST
 
 const Class = enum (u16) {
     IDE_Controller = 0x0101,
@@ -38,9 +25,9 @@ const Class = enum (u16) {
     Unknown = 0xFFFF,
 
     pub fn from_u16(value: u16) ?Class {
-        return kutil.int_to_enum(Class, value);
+        return kutil.int_to_enum(@This(), value);
     }
-
+    
     pub fn to_string(self: Class) []const u8 {
         return switch (self) {
             .IDE_Controller => "IDE Controller",
@@ -88,52 +75,204 @@ pub inline fn read_config16(bus: Bus, device: Device, function: Function,
 
 pub inline fn read_config8(bus: Bus, device: Device, function: Function,
         offset: Offset) u8 {
-    const x: u8 = @intCast(u8, (read_config16(
-        bus, device, function, offset) >> @intCast(u4, (offset * 8) & 0xF)) & 0xFFFF);
-    print.debug_format("{}\n", x);
-    return x;
+    return @intCast(u8, (read_config16(
+        bus, device, function, offset) >> @intCast(u4, (offset * 8) & 0xF)) & 0xFF);
 }
 
-pub inline fn get_vendor_id(bus: Bus, device: Device, function: Function) u16 {
-    return read_config16(bus, device, function, 0);
-}
+const Header = packed struct {
+    pub const Kind = packed enum (u8) {
+        Normal = 0x00,
+        MultiFunctionNormal = 0x80,
+        PciToPciBridge = 0x01,
+        MultiFunctionPciToPciBridge = 0x81,
+        CardBusBridge = 0x02,
+        MultiFunctionCardBusBridge = 0x82,
 
-pub inline fn get_class(bus: Bus, device: Device, function: Function) u16 {
-    return read_config16(bus, device, function, 0xB);
-}
-
-pub inline fn get_header_type(bus: Bus, device: Device, function: Function) u16 {
-    return read_config16(bus, device, function, 0xE);
-}
-
-pub inline fn check_function(bus: Bus, device: Device, function: Function) void {
-    const class_value: u16 = get_class(bus, device, function);
-    const class_maybe: ?Class = Class.from_u16(class_value);
-    if (class_maybe) |class| {
-        if (class == .PCI_To_PCI_Bridge) {
-            check_bus(read_config8(bus, device, function, 0x19));
-        } else {
-            print.debug_format(" - Found PCI Device: {} at ({}, {}, {})\n",
-                class.to_string(), bus, device, function);
+        pub fn from_u8(value: u8) ?Kind {
+            return kutil.int_to_enum(@This(), value);
         }
-    } else if (class_value != 0xffff) {
-        print.debug_format(" - Found Unknown PCI Device: {:x}  at ({}, {}, {})\n",
-            class_value, bus, device, function);
+
+        pub fn to_string(self: Kind) []const u8 {
+            return switch (self) {
+                .Normal => "Normal",
+                .MultiFunctionNormal => "Multi-Function Normal",
+                .PciToPciBridge => "PCI-to-PCI Bridge",
+                .MultiFunctionPciToPciBridge =>
+                    "Multi-Function PCI-to-PCI Bridge",
+                .CardBusBridge => "CardBus Bridge",
+                .MultiFunctionCardBusBridge =>
+                    "Multi-Function CardBus Bridge",
+            };
+        }
+        
+        pub fn is_multifunction(self: Kind) bool {
+            return switch (self) {
+                .MultiFunctionNormal => true,
+                .MultiFunctionPciToPciBridge => true,
+                .MultiFunctionCardBusBridge => true,
+                else => false,
+            };
+        }
+    };
+
+    pub const SuperClass = packed enum (u8) {
+        UnclassifiedDevice = 0x00,
+        MassStorageController = 0x01,
+        NetworkController = 0x02,
+        DisplayController = 0x03,
+        MultimediaController = 0x04,
+        MemoryController = 0x05,
+        BridgeDevice = 0x06,
+        SimpleCommunicationsController = 0x07,
+        GenericSystemPeripheral = 0x08,
+        InputDeviceController = 0x09,
+        DockingStation = 0x0A,
+        Processor = 0x0B,
+        SerialBusController = 0x0C,
+        WirelessController = 0x0D,
+        IntelligentController = 0x0E,
+        SatelliteCommunicationsController = 0x0F,
+        EncryptionController = 0x10,
+        SignalProcessingController = 0x11,
+        ProcessingAccelerator = 0x12,
+        NonEssentialInstrumentation = 0x13,
+        Coprocessor = 0x40,
+        
+        pub fn from_u8(value: u8) ?SuperClass {
+            return kutil.int_to_enum(@This(), value);
+        }
+
+        pub fn to_string(self: SuperClass) []const u8 {
+            return switch (self) {
+                .UnclassifiedDevice => "Unclassified Device",
+                .MassStorageController => "Mass Storage Controller",
+                .NetworkController => "Network Controller",
+                .DisplayController => "Display Controller",
+                .MultimediaController => "Multimedia Controller",
+                .MemoryController => "Memory Controller",
+                .BridgeDevice => "Bridge Device",
+                .SimpleCommunicationsController =>
+                    "Simple Communications Controller",
+                .GenericSystemPeripheral => "Generic System Peripheral",
+                .InputDeviceController => "Input Device Controller",
+                .DockingStation => "Docking Station",
+                .Processor => "Processor",
+                .SerialBusController => "Serial Bus Controller",
+                .WirelessController => "Wireless Controller",
+                .IntelligentController => "Intelligent Controller",
+                .SatelliteCommunicationsController =>
+                    "Satellite Communications Controller",
+                .EncryptionController => "Encryption Controller",
+                .SignalProcessingController => "Signal Processing Controller",
+                .ProcessingAccelerator => "Processing Accelerator",
+                .NonEssentialInstrumentation =>
+                    "Non-Essential Instrumentation",
+                .Coprocessor => "Coprocessor",
+            };
+        }
+    };
+
+    vendor_id: u16 = 0,
+    device_id: u16 = 0,
+    command: u16 = 0,
+    status: u16 = 0,
+    revision_id: u8 = 0,
+    prog_if: u8 = 0,
+    subclass: u8 = 0,
+    class: u8 = 0,
+    cache_line_size: u8 = 0,
+    latency_timer: u8 = 0,
+    header_type: u8 = 0,
+    bist: u8 = 0,
+
+    pub fn is_invalid(self: *const Header) bool {
+        return self.vendor_id == 0xFFFF;
+    }
+
+    pub fn get_header_type(self: *const Header) ?Kind {
+        return Kind.from_u8(self.header_type);
+    }
+
+    pub fn get_class(self: *const Header) ?SuperClass {
+        return SuperClass.from_u8(self.class);
+    }
+
+    pub fn print(self: *const Header, file: *io.File) io.FileError!void {
+        try fprint.format(file,
+            \\     - vendor_id: {:x}
+            \\     - device_id: {:x}
+            \\     - command: {:x}
+            \\     - status: {:x}
+            \\     - revision_id: {:x}
+            \\     - prog_if: {:x}
+            \\     - subclass: {:x}
+            \\     - class:
+            ,
+            self.vendor_id,
+            self.device_id,
+            self.command,
+            self.status,
+            self.revision_id,
+            self.prog_if,
+            self.subclass);
+        if (self.get_class()) |class| {
+            try fprint.format(file, " {}", class.to_string());
+        } else {
+            try fprint.format(file, " Unknown Class {:x}", self.class);
+        }
+        try fprint.format(file,
+            \\
+            \\     - cache_line_size: {:x}
+            \\     - latency_timer: {:x}
+            \\     - header_type:
+            ,
+            self.cache_line_size,
+            self.latency_timer);
+        if (self.get_header_type()) |header_type| {
+            try fprint.format(file, " {}", header_type.to_string());
+        } else {
+            try fprint.format(file, " Unknown Value {:x}", self.header_type);
+        }
+        try fprint.format(file,
+            \\
+            \\     - bist: {:x}
+            \\
+            , self.bist);
+    }
+
+    pub fn get(bus: Bus, device: Device, function: Function) Header {
+        const Self = @This();
+        var rv: [@sizeOf(Self)]u8 = undefined;
+        for (rv[0..]) |*ptr, i| {
+            ptr.* = read_config8(bus, device, function, @intCast(u8, i));
+        }
+        return @bitCast(Self, rv);
+    }
+};
+
+pub inline fn check_function(bus: Bus, device: Device, function: Function, header: *const Header) void {
+    if (header.is_invalid()) return;
+    print.format("   - Bus {}, Device {}, Function {}\n", bus, device, function);
+    _ = header.print(print.get_console_file().?) catch {};
+    if (header.get_class()) |class| {
+        if (class == .BridgeDevice and header.subclass == 0x04) {
+            check_bus(read_config8(bus, device, function, 0x19));
+        }
     }
 }
 
 pub inline fn check_device(bus: Bus, device: Device) void {
-    if (get_vendor_id(bus, device, 0) == 0xFFFF) return;
-    check_function(bus, device, 0);
-    const is_multi: bool = get_header_type(bus, device, 0) & 0x0080 != 0;
-    if (is_multi) {
-        // Header Type is Multi-Function, Check Them
-        var i: Function = 1;
-        while (true) : (i += 1) {
-            if (get_vendor_id(bus, device, i) != 0xFFFF) {
-                check_function(bus, device, i);
+    const header = Header.get(bus, device, 0);
+    check_function(bus, device, 0, &header);
+    if (header.get_header_type()) |header_type| {
+        if (header_type.is_multifunction()) {
+            // Header Type is Multi-Function, Check Them
+            var i: Function = 1;
+            while (true) : (i += 1) {
+                const subheader = Header.get(bus, device, i);
+                check_function(bus, device, i, &subheader);
+                if (i == 7) break;
             }
-            if (i == 7) break;
         }
     }
 }
@@ -147,5 +286,6 @@ pub fn check_bus(bus: u8) void {
 }
 
 pub fn find_pci_devices() void {
+    print.string(" - Seaching for PCI Devices\n");
     check_bus(0);
 }
